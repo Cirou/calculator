2d288b3ad5060a31766be71553507c995a99c44f
Francesca Profeta <francesca.profeta@accenture.com>
2018-03-26 18:10:44 +0200

    Test Signed-off-by: Francesca Profeta <francesca.profeta@accenture.com>

--- src/main/java/net/cirou/calculator/controller/CalculatorController.java
+++ src/main/java/net/cirou/calculator/controller/CalculatorController.java
@@ -1,8 +1,11 @@
 package net.cirou.calculator.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,16 +14,29 @@ import net.cirou.calculator.database.SQLiteJDBCDriverConnection;
 @Controller
 public class CalculatorController{
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorController.class);
+
 	@Autowired
 	SQLiteJDBCDriverConnection sql;
-	
-    @GetMapping("/calculator")
-    @ResponseBody
-    public String sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
-    	
-    	sql.connect();
-    	
-        return "########\r\n########\r\n";
-    }
-    
+
+	@GetMapping("/calculator")
+	@ResponseBody
+	public String sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+
+		sql.connect();
+
+		return "########\r\n########\r\n";
+	}
+
+	@GetMapping("/calculator/add/{numero1}/{numero2}")
+	@ResponseBody
+	public String addiziona(@PathVariable("numero1") String numero1, @PathVariable("numero2") String numero2) {
+
+		sql.connect();
+		
+		return "risultato"; 
+
+
+	}
 }
+
