1cc3f207d60f63f32948e1cb8afa3ab009b2cd23
Francesca Profeta <francesca.profeta@accenture.com>
2018-03-27 14:50:11 +0200

    Test commit
    Signed-off-by: Francesca Profeta <francesca.profeta@accenture.com>

--- src/main/java/net/cirou/calculator/controller/CalculatorController.java
+++ src/main/java/net/cirou/calculator/controller/CalculatorController.java
@@ -1,5 +1,9 @@
 package net.cirou.calculator.controller;
 
+import javax.xml.bind.ValidationException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import net.cirou.calculator.database.SQLiteJDBCDriverConnection;
 
 @Controller
-public class CalculatorController{
+public class CalculatorController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorController.class);
 
@@ -21,7 +25,7 @@ public class CalculatorController{
 
 	@GetMapping("/calculator")
 	@ResponseBody
-	public String sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+	public String sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
 
 		sql.connect();
 
@@ -31,12 +35,158 @@ public class CalculatorController{
 	@GetMapping("/calculator/add/{numero1}/{numero2}")
 	@ResponseBody
 	public String addiziona(@PathVariable("numero1") String numero1, @PathVariable("numero2") String numero2) {
-
-		sql.connect();
 		
-		return "risultato"; 
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return addizionaNumeri(numero1, numero2);
+	}
 
+	@GetMapping("/calculator/add2")
+	@ResponseBody
+	public String addiziona2(@RequestParam("numero1") String numero1, @RequestParam("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return addizionaNumeri(numero1, numero2);
+	}
+
+	private String addizionaNumeri(String numero1, String numero2) {
+
+		int numeroa = Integer.parseInt(numero1);
+		int numerob = Integer.parseInt(numero2);
+
+		return "risultato =" + (numeroa + numerob);
+
+	}
+	
+	@GetMapping("/calculator/multiply/{numero1}/{numero2}")
+	@ResponseBody
+	public String moltiplica(@PathVariable("numero1") String numero1, @PathVariable("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return moltiplicaNumeri(numero1, numero2);
+	}
+
+	@GetMapping("/calculator/multiply2")
+	@ResponseBody
+	public String prodotto2(@RequestParam("numero1") String numero1, @RequestParam("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return moltiplicaNumeri(numero1, numero2);
+	}
+
+	private String moltiplicaNumeri(String numero1, String numero2) {
+
+		int numeroa = Integer.parseInt(numero1);
+		int numerob = Integer.parseInt(numero2);
+
+		return "risultato =" + (numeroa * numerob);
+
+	}
+	
+	@GetMapping("/calculator/numerator/{numero1}/{numero2}")
+	@ResponseBody
+	public String dividi(@PathVariable("numero1") String numero1, @PathVariable("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return dividiNumeri(numero1, numero2);
+	}
+
+	@GetMapping("/calculator/numerator")
+	@ResponseBody
+	public String dividi2(@RequestParam("numero1") String numero1, @RequestParam("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return dividiNumeri(numero1, numero2);
+	}
+
+	private String dividiNumeri(String numero1, String numero2) {
+
+		int numeroa = Integer.parseInt(numero1);
+		int numerob = Integer.parseInt(numero2);
+
+		return "risultato =" + (numeroa / numerob);
+
+	}
+	
+	@GetMapping("/calculator/subtraction/{numero1}/{numero2}")
+	@ResponseBody
+	public String sottrai(@PathVariable("numero1") String numero1, @PathVariable("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return sottraiNumeri(numero1, numero2);
+	}
+
+	@GetMapping("/calculator/subtraction")
+	@ResponseBody
+	public String sottrai2(@RequestParam("numero1") String numero1, @RequestParam("numero2") String numero2) {
+		
+		try {
+			validazione(numero1, numero2);
+		} catch (ValidationException e) {
+			LOGGER.error(e.getMessage());
+			return e.getMessage();
+		}
+		
+		return sottraiNumeri(numero1, numero2);
+	}
+
+	private String sottraiNumeri(String numero1, String numero2) {
+
+		int numeroa = Integer.parseInt(numero1);
+		int numerob = Integer.parseInt(numero2);
+
+		return "risultato =" + (numeroa - numerob);
+
+	}
+
+
+	private void validazione(String numero1, String numero2) throws ValidationException {
+
+		if (StringUtils.isEmpty(numero1) || StringUtils.isEmpty(numero2) 
+				&& !NumberUtils.isDigits(numero1) || !NumberUtils.isDigits(numero2)) {
+			throw new ValidationException("formato non valido");
+		}
 
 	}
 }
-
